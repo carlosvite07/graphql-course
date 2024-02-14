4e064d7007b5c3c826501a27c09da7d2b31bd23a
carlosvite07 <carlosvite07@gmail.com>
2024-02-14 13:36:43 -0600

    Creacion de usuario con password hasheado Video 27

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,7 +1,35 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+
 //Resolvers
 const resolvers = {
   Query: {
     obtenerCurso: () => "Algo",
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // Revisar si el usuario ya esta registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error("El usuario ya esta registrado");
+      }
+      console.log(existeUsuario);
+
+      // Hashear su password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      try {
+        // Guardarlo en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save(); // guardarlo
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 
