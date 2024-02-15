e09aa53ec9b4b42d5c13f51035abc83ec3ca3119
carlosvite07 <carlosvite07@gmail.com>
2024-02-15 12:24:05 -0600

    CRUD de Cliente Video 46

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const {
 } = require("apollo-server-core");
 const typeDefs = require("./db/schema");
 const resolvers = require("./db/resolvers");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
 
 const conectarDB = require("./config/db");
 
@@ -14,6 +16,20 @@ conectarDB();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    // console.log(req.headers["authorization"]);
+    const token = req.headers["authorization"] || "";
+    if (token) {
+      try {
+        const usuario = await jwt.verify(token, process.env.SECRETA);
+        // console.log(usuario);
+        return { usuario };
+      } catch (error) {
+        console.log("Hubo un error");
+        console.log(error);
+      }
+    }
+  },
   plugins: [
     // deshabilitar el plugin de informes de uso
     ApolloServerPluginUsageReportingDisabled(),
